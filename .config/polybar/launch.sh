075876d95e8c8b8640777c357aa229ae7fb4db0d
Tim Kaelble <tk@tower>
2022-12-30 11:32:29 +0100

    fix polybar for two screens

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,10 +5,16 @@
 # polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 killall -q polybar
-
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar1 &
+  done
+else
+  polybar --reload bar1 &
+fi
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar bottom 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
