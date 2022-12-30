f53765ee48446c975b8f5512817bf8ae9f715fd3
Tim Kaelble <tk@tower>
2022-12-30 11:05:40 +0100

    initial commit

--- .config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-alias nvim="/opt/nvim.appimage"
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-starship init fish | source
-set fish_greeting
